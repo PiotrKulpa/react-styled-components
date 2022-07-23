c640c8239d29039b8894d893f4a61e8e89682fc1
piotr <info.kulpa@gmail.com>
2022-07-23 10:37:16 +0200

    implement slider

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,16 +1,41 @@
-import React, {FC} from 'react';
+import React, { FC } from 'react';
 import styled from 'styled-components';
+import Slider from 'react-slick';
+import { SliderWrapper, SliderPattern } from '../components/styles';
 
-const Title = styled.h1`
-  font-size: 1.5em;
-  text-align: center;
-  color: ${({theme}) => theme.colors.grey};
-`;
+const settings = {
+  dots: false,
+  arrows: false,
+  infinite: true,
+  autoplay: true,
+  speed: 500,
+  autoplaySpeed: 3000,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  pauseOnHover: false,
+};
+
+const sliders: { text: string; imgUrl: string }[] = [
+  { text: 'WELCOME TO MY UNIVERSE', imgUrl: '/1.jpg' },
+  { text: 'CREATIVITY. INNOVATION', imgUrl: '/2.jpg' },
+  { text: 'CHECK ALL MY WORKS', imgUrl: '/3.jpg' },
+];
 
 const Home: FC = () => {
   return (
-    <div><Title>Home</Title></div>
-  )
-}
+    <div>
+      <Slider {...settings}>
+        {sliders.map(({ text, imgUrl }) => (
+          <>
+            <SliderPattern />
+            <SliderWrapper {...{ imgUrl }}>
+              <h2>{text}</h2>
+            </SliderWrapper>
+          </>
+        ))}
+      </Slider>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
--- src/components/styles.ts
+++ src/components/styles.ts
@@ -1,3 +1,4 @@
+import { url } from 'inspector';
 import styled from 'styled-components';
 
 interface MenuWrapperProps {
@@ -9,8 +10,13 @@ interface MenuItemWrapperProps {
   readonly key: number;
 }
 
+interface SliderWrapperProps {
+  readonly imgUrl: string;
+}
+
 export const MenuWrapper = styled.ul<MenuWrapperProps>`
-  margin: 0;
+  margin: 20px 0 0;
+  position: sticky;
   display: flex;
   list-style-type: none;
   align-items: center;
@@ -36,5 +42,35 @@ export const MenuItemWrapper = styled.li<MenuItemWrapperProps>`
   a {
     color: ${({isHome, theme}) => isHome ? theme.colors.white : theme.colors.black};
     text-decoration: none;
+  }
+`;
+
+export const SliderPattern = styled.div`
+position: absolute;
+  height: 100vh;
+  width: 100vw;
+  background-image:  url('pattern.png');
+`;
+
+export const SliderWrapper = styled.div<SliderWrapperProps>`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-image:  url(${props => props.imgUrl});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 100vh;
+  background-color: ${({theme}) => theme.colors.white};
+
+  h2 {
+    font-weight: 700;
+    font-size: 5em;
+    letter-spacing: -0.05em;
+    margin: 0px;
+    text-transform: uppercase;
+    line-height: 1.05em;
+    color: ${({theme}) => theme.colors.white};
   }
 `;
